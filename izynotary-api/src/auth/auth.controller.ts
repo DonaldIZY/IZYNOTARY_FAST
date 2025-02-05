22b259547d285f00e80c5bcbb8ea1ede2f958549
izyherve <izyherve@gmail.com>
2025-02-05 15:03:25 +0000

    add super_admin

--- izynotary-api/src/auth/auth.controller.ts
+++ izynotary-api/src/auth/auth.controller.ts
@@ -1,35 +1,21 @@
-import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common'; //+
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { Response } from 'express'; //+
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) {}
 
-  @Post('login')
-  async login(
-    @Body('email') email: string,
-    @Body('password') password: string,
-    @Res() res: Response,
-  ) {
+    @Post('login')
+    async login(@Body('email') email: string, @Body('password') password: string) {
+        return this.authService.login(email, password);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('protected')
+    protectedRoute(@Req() req: any) {
+        return { message: 'This is a protected route', user: req.user };
+    }
+
     
-    const accessToken = await this.authService.login(email, password, res);
-
-    // Définir le cookie sécurisé (httpOnly, sécurisé)
-    res.cookie('access_token', accessToken, {
-      httpOnly: true, // Empêche l'accès JavaScript (sécurité XSS)
-      //secure: process.env.NODE_ENV === 'production', // Active `secure` en prod (HTTPS obligatoire)
-      sameSite: 'strict', // Empêche les attaques CSRF
-      maxAge: 24 * 60 * 60 * 1000, // Expire après 1 jour
-    });
-
-    return res.json({ message: 'Connexion réussie' });
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('protected')
-  protectedRoute(@Req() req: any) {
-    return { message: 'This is a protected route', user: req.user };
-  }
 }
--- izynotary-api/src/auth/auth.service.ts
+++ izynotary-api/src/auth/auth.service.ts
@@ -4,34 +4,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Password } from 'src/utils/password.util';
 import { Repository } from 'typeorm';
-import { Response } from 'express'; // 🔥 Ajout pour manipuler la réponse HTTP
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-    private readonly jwtService: JwtService,
-  ) {}
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        private readonly jwtService: JwtService,
+    ) {}
 
-  async login(email: string, password: string, res: Response) {
-    const user = await this.userRepository.findOne({
-      where: { email },
-      relations: {
-        role: true,
-        identifier: true,
-      },
-    });
+    async login(email: string, password: string): Promise<{ accessToken: string }> {
+        const user = await this.userRepository.findOne({ 
+            where: { email },
+            relations: {
+                role: true, 
+                identifier: true
+            }
+        });
 
-    if (
-      !user ||
-      !(await Password.validatePassword(password, user.identifier.hashedValue))
-    ) {
-      throw new UnauthorizedException('Invalid credentials');
+        if(!user || !(await Password.validatePassword(password, user.identifier.hashedValue))) {
+            throw new UnauthorizedException('Invalid credentials')
+        }
+
+        const payload = { id: user.id, email: user.email };
+        const accessToken = this.jwtService.sign(payload);
+
+        return { accessToken };
     }
-
-    const payload = { id: user.id, email: user.email };
-    return this.jwtService.sign(payload);
-
-  }
 }
