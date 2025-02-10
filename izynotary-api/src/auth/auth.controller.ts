b4870c27b577be7edabb1ab74ec4273660cec2e0
Izyyohann <izyyohanncoulibaly@gmail.com>
2025-02-10 14:59:13 +0000

    Création de la route pour les statistiques

--- izynotary-api/src/auth/auth.controller.ts
+++ izynotary-api/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
@@ -7,8 +8,8 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('login')
-    async login(@Body('email') email: string, @Body('password') password: string) {
-        return this.authService.login(email, password);
+    async login(@Body('email') email: string, @Body('password') password: string, @Res() res: Response) {
+        return this.authService.login(email, password, res);
     }
 
     @UseGuards(JwtAuthGuard)
